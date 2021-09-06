b2449ac4d6a5f14577fb1ff3f4e9bd0721564f5c
edwardTunkl <eddytunkl@googlemail.com>
2021-09-06 16:25:39 +0200

    About finished

--- linkedin_clone_build_week/src/Components/Home.jsx
+++ linkedin_clone_build_week/src/Components/Home.jsx
@@ -1,5 +1,7 @@
 import { Row, Container, Col } from "react-bootstrap";
+import About from "./About";
 import Dashboard from "./Dashboard";
+
 
 const Home = () => {
   return (
@@ -8,6 +10,7 @@ const Home = () => {
         <Col sm={12} md={9}>
           <h1>MainContainer</h1>
           <Dashboard />
+          <About/>
         </Col>
         <Col sm={0} md={3}>
           <h1>Aside</h1>
