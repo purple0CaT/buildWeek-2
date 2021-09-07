2d7134231f4a0f3d10fe5a53dd3c731f3be8c863
Istvan <steven.ford.j@gmail.com>
2021-09-07 13:52:07 +0300

    Fix id

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Home from "./Components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Container>
+          <Route path="/" exact component={Home} />
+          <Route path="/home/:id" exact component={Home} />
+        </Container>
+      </Router>
     </div>
   );
 }
