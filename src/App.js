86b450f731d6b4b61ff560585f435f70553551ae
AzizbekTokhirjonov <83159174+AzizbekTokhirjonov@users.noreply.github.com>
2021-09-08 10:23:48 +0200

    setting feed

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from "./Components/Footer";
 import LinkedinNavbar from "./Components/Navbar";
-import { Container } from "react-bootstrap";
+import Feed from "./Components/Feed";
+
 function App() {
   return (
-      <Router>
-          <LinkedinNavbar />
-          <Route path="/" exact component={Home} />
-          <Route path="/home/:id" exact component={Home} />
-          <Footer />
-      </Router>
+    <Router>
+      <LinkedinNavbar />
+      <Route path="/" exact component={Feed} />
+      <Route path="/feed" exact component={Feed} />
+      <Route path="/home/:id" exact component={Home} />
+      <Footer />
+    </Router>
   );
 }
 
